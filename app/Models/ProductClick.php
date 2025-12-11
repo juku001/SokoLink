cbbea034e410d7c0d46d8a20ed6e0cce54183ccf
juku_001 <jumakassim89@gmail.com>
2025-12-11 14:15:06 +0300

    v1.5.6(reports fixes)

--- app/Models/ProductClick.php
+++ app/Models/ProductClick.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductClick extends Model
 {
-    //
+    protected $fillable = [
+        'product_id'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
