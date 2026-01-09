b22173b5c002765ab1cd75de80b2b37be374afb8
juku_001 <jumakassim89@gmail.com>
2026-01-09 16:46:10 +0300

    v3.0.0(fixed product upload)

---
+++ database/migrations/2026_01_09_134525_alter_price_column_on_products_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('price', 12, 2)->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('price', 8, 2)->change();
+        });
+    }
+};
